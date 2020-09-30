e33b4cd4ff83ed63dd226025cb8e124340140609
zengpan15 <71753081+zengpan15@users.noreply.github.com>
2020-09-30 21:03:03 +0800

    Add files via upload

---
+++ Area/src/test2/Examplel_2.java
@@ -0,0 +1,12 @@
+package test2;
+
+class Examplel_2 {
+    public static void main(String args[]){
+        Rect rectangle;
+        rectangle=new Rect();
+        rectangle.width=1.819;
+        rectangle.height=1.5;
+        double area=rectangle.getArea();
+        System.out.println("矩形的面积:"+area);
+    }
+}
---
+++ Area/src/test2/Rect.java
@@ -0,0 +1,9 @@
+package test2;
+
+public class Rect {
+    double width;
+    double height;
+    double getArea(){
+        return width*height;
+    }
+}
